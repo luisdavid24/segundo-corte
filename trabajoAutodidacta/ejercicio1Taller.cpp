a32c5c64d23858a5d95d8d4f8a295ab7cf6fd37b
Luis david <david12.29@hotmail.com>
2020-06-10 20:24:08 -0500

    sigo luchando

--- trabajoAutodidacta/ejercicio1Taller.cpp
+++ trabajoAutodidacta/ejercicio1Taller.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-int n,indice;
-int *vector;
+int n,indice,j;
+int *vector,*vectorFinal;
 using namespace std;
 void llenar(int *vector,int n)
 {	for(int i=0;i<n;i++)
@@ -15,31 +15,55 @@ void mostrar(int *vector,int n)
 	}
 }
 
-void ordenar(int n,int indice)
-{	int i=0,j=0,aux=0;
-	for(i=0;n-1;i++)
-	{
-		for(j=i+1;n;i++)
-		{
-			if(vector[i]>vector[j] and vector[j]%2==0)
-			{
-				aux=vector[i];
-				vector[i]=vector[j];
-				vector[j]=aux;
-				indice++;
-			}
+void ordenar(int *vector,int *vectorFinal,int n)
+{	int i,k,aux;
+	j=0;
+	for(i=0;i<n;i++)
+	{	if(vector[i]%2==0)	
+		{	vectorFinal[j]=vector[i];
+			j++;
 		}
+		
+	}
+	
+	k=j;
+	for(i=0;i<n;i++)
+	{	if(vector[i]%2!=0)	
+		{	vectorFinal[k]=vector[i];
+			k++;
+		}
+		
 	}
 }
-
+void ordenamiento(int *vectorFinal,int n)
+{	int k,i,aux;
+	for(i=1;i<j-1;i++)
+	{	for(k=i+1;j;k++)
+		{	if(vectorFinal[i]>vectorFinal[k])
+			{	aux=vectorFinal[k];
+				vectorFinal[i]=vectorFinal[k];
+				vectorFinal[k]=aux;
+			}
+			
+		}
+		
+	}
+	
+}
 int main(int argc, char** argv) {
 	cout<<"Por favor poner la cantidad de elementos del vector: ";
 	cin>>n;
 	vector=new int(n);
-	llenar(vector,n);
+	vectorFinal=new int(n);
 	
-//	ordenar(n,indice);
-	mostrar(vector,n);
+	llenar(vector,n);
+	ordenar(vector,vectorFinal,n);
+	mostrar(vectorFinal,n);
+	cout<<"Este el valor de j :"<<j;
+	
+	ordenamiento(vectorFinal,n);
+	cout<<endl;
+	mostrar(vectorFinal,n);
 	return 0;
 	
 }
