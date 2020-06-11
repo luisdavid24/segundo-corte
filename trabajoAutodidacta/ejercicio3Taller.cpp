a128b55022182cce9eee0ff943ec26f90bfc87da
Luis david <david12.29@hotmail.com>
2020-06-11 16:12:55 -0500

    ya funciona

--- trabajoAutodidacta/ejercicio3Taller.cpp
+++ trabajoAutodidacta/ejercicio3Taller.cpp
@@ -2,57 +2,48 @@
 #include <stdlib.h>
 #include <vector>
 int n1,m1,n2,m2;
-int **matriz1,**matriz2,**matriz3;
-
 using namespace std;
 
-void llenar(int **matriz,int n,int m)
-{	matriz=new int*[n];
-	for(int i=0;i<n;i++)
-	{	matriz[i]=new int[m];
+
+int main(int argc, char** argv) {
+	cout<<"Vamos a llenar la primera matriz"<<endl;
+	int matriz1[3][3];
+	for(int i=0;i<3;i++)
+	{	for(int j=0;j<3;j++)
+		{
+			cout<<"Poner el valor en la posicion ["<<i<<"]["<<j<<"] : ";
+			cin>>matriz1[i][j];
+		}
 		
 	}
-	for(int i=0;i<n;i++)
-	{	
-		for(int j=0;j<m;j++)
-		{	cout<<"Escriba el valor en la posicion ["<<i<<"]["<<j<<"]: ";
-			cin>>*(*(matriz+i)+j);
+	cout<<"Vamos a llenar la segunda  matriz"<<endl;
+	int matriz2[3][3];
+	for(int i=0;i<3;i++)
+	{	for(int j=0;j<3;j++)
+		{	cout<<"Poner el valor en la posicion ["<<i<<"]["<<j<<"] : ";
+			cin>>matriz2[i][j];
 		}
 	}
-}
-void mostrar(int **matriz,int n,int m)
-{	
-	for(int i=0;i<n;i++)
-	{	for(int j=0;j<m;j++)
-		{	cout<<*(*(matriz+i)+j)<<" ";
-			cout<<"Entro en el bucle";
+	int matriz3[3][3];
+	for(int i=0;i<3;i++)
+	{	for(int j=0;j<3;j++)
+		{	matriz3[i][j]=0;
+		}
+	}
+	//Aqui va la multiplicacion
+	for(int i=0;i<3;i++)
+	{	for(int j=0;j<3;j++)
+		{	for(int z=0;z<3;z++)
+			{	matriz3[i][j]+=matriz1[i][z]*matriz2[z][j];
+			}
+		}
+	}
+	//mostrar 
+	for(int i=0;i<3;i++)
+	{	for(int j=0;j<3;j++)
+		{	cout<<matriz3[i][j]<<" ";
 		}
 		cout<<endl;
 	}
-}
-void multiplicar()
-{	for(int i=0;i<n1;i++)
-	{	for(int j=0;j<m2;j++)
-		{	for(int z=0;z<n2;++z)
-			{	*(*(matriz3+i)+j)+=(*(*(matriz1+i)+j))*(*(*(matriz2+i)+j));
-			}
-		}
-		
-	}
-	cout<<"Entro";
-}
-int main(int argc, char** argv) {
-
-	cout<<"Escribir el numero de filas de la matriz: ";
-	cin>>n1;
-	cout<<"Escribir el numero de columnas de la  matriz: ";
-	cin>>m1;
-	llenar(matriz1,n1,m1);
-	mostrar(matriz1,n1,m1);
-	
-//	mostrar(matriz1,n1,m1);
-
-
-
 	return 0;
 }
